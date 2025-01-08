f88092733ebb72b8082e57f9e1a4ab24b114b8b2
muhammad-jasim-m <jasimdev200@gmail.com>
2025-01-08 16:22:33 +0500

    updated code

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Section1.css";
 import section1 from "../../Asserts/img/section1.png"
 import { useDarkMode } from '../../Context/Darkcontext';
+import { Link } from 'react-scroll';
 
 function Section1() {
 
@@ -14,7 +15,13 @@ function Section1() {
           <div className='onehearding'>Transforming Ideas Into Digital Excellence</div>
           <div className='oneparagraph' style={{color :darkMode ?"white":""}}>At E-Dev Alliance, we specialize in crafting cutting-edge websites, eye-catching designs, and professional video and photo editing services to elevate your brand's online presence</div>
           <div style={{color :darkMode ?"white":""}}>
-            <div>Explore Our Services</div>
+            <div><Link
+                          to="services"
+                          smooth={true}
+                          duration={500}
+                          spy={true}
+                         
+                        >Explore Our Services </Link></div>
             <div>Get a Free Quote</div></div>
         </div>
         <div className="oneright">
