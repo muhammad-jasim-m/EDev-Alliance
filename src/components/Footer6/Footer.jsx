d2e21756baf62b1d528e2bef804f18042a4a9ac5
muhammad-jasim-m <jasimdev200@gmail.com>
2025-01-25 19:13:31 +0500

    section one

--- src/components/Footer6/Footer.jsx
+++ src/components/Footer6/Footer.jsx
@@ -1,10 +1,90 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../Asserts/img/Image.png';
+import { Icon } from '@iconify/react';
+import "./Footer.css";
+import { useDarkMode } from '../../Context/Darkcontext';
+import { Link } from 'react-scroll';
+
 
 function Footer() {
+
+  const {darkMode} = useDarkMode();
+
   return (
     <>
-    <div className="footermaindiv">
-        this is the footer of the website
+    <div className="footermaindiv" style={{ background: darkMode ? "black" : "#fff" }}>
+        
+         <div className="logofirst">
+                  <img src={logo} alt="" />
+                  EDev Alliance
+                </div>
+
+<div className="decfooter" style={{ color: darkMode ? "white" : "" }}>
+At E-Dev Alliance, we specialize in crafting cutting-edge websites, eye-catching designs, and professional video and photo editing services to elevate your brand's online presence
+</div>
+
+<div className="socialiconsfooter">
+            <div><Icon icon="ic:outline-whatsapp" width="24" height="24" style={{ color: "#fff" }} /></div>
+            <div><Icon icon="mdi:email" width="24" height="24" style={{ color: "#fff" }} /></div>
+            <div><Icon icon="mingcute:linkedin-fill" width="24" height="24" style={{ color: "#fff" }} /></div>
+          </div>
+
+
+<div className="logosecond" id='footerlinks'>
+            <ul style={{ color: darkMode ? "white" : "" }}>
+              <li><Link
+                to="home"
+                smooth={true}
+                duration={500}
+                spy={true}
+                // onClick={handleLinkClick}
+                activeClass="active-link"
+              >
+                Home
+              </Link> </li>
+              <li><Link
+                to="about"
+                smooth={true}
+                duration={500}
+                spy={true}
+                // onClick={handleLinkClick}
+                activeClass="active-link"
+              >
+                About Us
+              </Link> </li>
+              <li> <Link
+                to="services"
+                smooth={true}
+                duration={500}
+                spy={true}
+                // onClick={handleLinkClick}
+                activeClass="active-link"
+              >
+                Services
+              </Link></li>
+              <li><Link
+                to="projects"
+                smooth={true}
+                duration={500}
+                spy={true}
+                // onClick={handleLinkClick}
+                activeClass="active-link"
+              >
+                Projets
+              </Link></li>
+              <li><Link
+                to="testimonial"
+                smooth={true}
+                duration={500}
+                spy={true}
+                // onClick={handleLinkClick}
+                activeClass="active-link"
+              >
+                Testimonals
+              </Link></li>
+            </ul>
+          </div>
+
     </div>
     
     </>
